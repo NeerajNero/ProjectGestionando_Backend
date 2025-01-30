a332fc070bbebe4adfc8945d01331d658e24856b
NeerajNero <neerajnero9066@gmail.com>
2025-01-30 20:37:29 +0530

    updated for porduction

--- middleware/authCheck.js
+++ middleware/authCheck.js
@@ -7,7 +7,7 @@ export const authCheck = async(req,res,next) => {
             return res.status(401).json("Inavlid token")
         }
         const extractedToken = token.split(" ")[1];
-        const checkToken = jwt.verify(extractedToken, process.env.SECRET_KEY)
+        const checkToken = jwt.verify(token, process.env.SECRET_KEY)
         if(!checkToken){
             return res.status(403).json({error: "Invalid Token"})
         }
